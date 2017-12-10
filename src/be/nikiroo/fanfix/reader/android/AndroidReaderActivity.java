a18ba53682025b3de2db4ae599f1e9e35c3002b5
Niki Roo <niki@nikiroo.be>
2017-12-10 17:59:13 +0100

    Android: we can now download and display the list

--- src/be/nikiroo/fanfix/reader/android/AndroidReaderActivity.java
+++ src/be/nikiroo/fanfix/reader/android/AndroidReaderActivity.java
@@ -1,11 +1,13 @@
 package be.nikiroo.fanfix.reader.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 
@@ -25,70 +27,88 @@ public class AndroidReaderActivity extends Activity implements
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		config();
+		reader = config();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
-	int i = 1;
+	@Override
+	protected void onStart() {
+		super.onStart();
+		refresh();
+	}
 
-	public void buttonClick(View view) {
-		AndroidReaderGroup group = null;
-		if (group == null) {
-			group = new AndroidReaderGroup();
-		}
+	private void refresh() {
+		AndroidReaderGroup group = new AndroidReaderGroup();
 
 		FragmentTransaction trans = getFragmentManager().beginTransaction();
-		trans.replace(R.id.dropZone, group);
+		trans.replace(R.id.Main_pnlStories, group);
 		trans.commit();
 		getFragmentManager().executePendingTransactions();
 
-		group.fill(reader, null);
+		group.fill(reader.getLibrary().getList(), reader);
 	}
 
-	public void onClick(View view) {
-		new AsyncTask<Void, Void, String>() {
-			@Override
-			protected void onPreExecute() {
-				EditText editText = findViewById(R.id.editText);
-				editText.setText("Downloading...");
-			}
+	public void onAdd(View view) {
+		final View root = findViewById(R.id.Main);
 
-			@Override
-			protected String doInBackground(Void... voids) {
-				try {
-					URL[] urls = new URL[] {
-							new URL("https://e621.net/pool/show/13124"),
-							new URL("https://e621.net/pool/show/13121"), };
+		ask(this,
+				"Import new story",
+				"Enter the story URL (the program will then download it -- the interface will not be usable until it is downloaded",
+				"Download", new AnswerListener() {
+					@Override
+					public void onAnswer(final String answer) {
+						root.setEnabled(false);
+						new Thread(new Runnable() {
+							@Override
+							public void run() {
+								try {
+									URL url = new URL(answer);
+									reader.getLibrary().imprt(url, null);
+								} catch (Throwable e) {
+									// TODO: show error message correctly
+									String mess = "";
+									for (String tab = ""; e != null
+											&& e != e.getCause(); e = e
+											.getCause()) {
+										mess += tab + "["
+												+ e.getClass().getSimpleName()
+												+ "] " + e.getMessage() + "\n";
+										tab += "\t";
+									}
 
-					for (int i = 0; i < urls.length; i++) {
-						if (reader.getLibrary().getList().size() <= i) {
-							reader.getLibrary().imprt(urls[i], null);
-						}
+									final String messf = mess;
+									AndroidReaderActivity.this
+											.runOnUiThread(new Runnable() {
+												@Override
+												public void run() {
+													ask(AndroidReaderActivity.this,
+															"Error",
+															"Cannot import URL: \n"
+																	+ messf,
+															"OK", null);
+												}
+											});
+
+								}
+
+								AndroidReaderActivity.this
+										.runOnUiThread(new Runnable() {
+											@Override
+											public void run() {
+												refresh();
+												root.setEnabled(true);
+											}
+										});
+							}
+						}).start();
 					}
+				});
 
-					String message = "";
-					for (MetaData meta : reader.getLibrary().getList()) {
-						message += meta.getTitle() + "\n";
-					}
-
-					return message;
-				} catch (Exception e) {
-					return e.getClass() + ": " + e.getMessage();
-				}
-			}
-
-			@Override
-			protected void onPostExecute(String message) {
-				EditText editText = findViewById(R.id.editText);
-				editText.setText("testy");
-
-				Intent intent = new Intent(AndroidReaderActivity.this,
-						SayIt.class);
-				intent.putExtra(SayIt.MESSAGE, message);
-				startActivity(intent);
-			}
-		}.execute();
+		/*
+		 * Intent intent = new Intent(AndroidReaderActivity.this, SayIt.class);
+		 * intent.putExtra(SayIt.MESSAGE, message); startActivity(intent);
+		 */
 	}
 
 	@Override
@@ -101,9 +121,9 @@ public class AndroidReaderActivity extends Activity implements
 		}
 	}
 
-	private void config() {
+	private Reader config() {
 		if (reader != null) {
-			return;
+			return reader;
 		}
 
 		String internal = getExternalFilesDir(null).toString();
@@ -130,6 +150,42 @@ public class AndroidReaderActivity extends Activity implements
 		Instance.setTraceHandler(new TraceHandler(true, true, 2));
 
 		BasicReader.setDefaultReaderType(Reader.ReaderType.ANDROID);
-		reader = BasicReader.getReader();
+		return BasicReader.getReader();
+	}
+
+	public static void ask(Context context, String title, String message,
+			String okMessage, final AnswerListener listener) {
+		final EditText input = new EditText(context);
+		input.setFocusable(true);
+		input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+		AlertDialog.Builder alert = new AlertDialog.Builder(context);
+		alert.setTitle(title);
+		alert.setMessage(message);
+		alert.setCancelable(true);
+		alert.setView(input);
+
+		if (listener != null) {
+			alert.setPositiveButton(okMessage,
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							listener.onAnswer(input.getText().toString());
+						}
+					});
+
+			alert.setOnCancelListener(new DialogInterface.OnCancelListener() {
+				@Override
+				public void onCancel(DialogInterface dialog) {
+					listener.onAnswer(null);
+				}
+			});
+		}
+
+		alert.show();
+	}
+
+	private interface AnswerListener {
+		public void onAnswer(String answer);
 	}
 }
--- src/be/nikiroo/fanfix/reader/android/AndroidReaderGroup.java
+++ src/be/nikiroo/fanfix/reader/android/AndroidReaderGroup.java
@@ -3,13 +3,17 @@ package be.nikiroo.fanfix.reader.android;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import be.nikiroo.fanfix.data.MetaData;
 import be.nikiroo.fanfix.reader.Reader;
@@ -21,6 +25,7 @@ import be.nikiroo.fanfix.reader.Reader;
  */
 public class AndroidReaderGroup extends Fragment {
 	private OnFragmentInteractionListener listener;
+	private Map<View, AndroidReaderBook> books = new HashMap<View, AndroidReaderBook>();
 
 	public interface OnFragmentInteractionListener {
 		void onFragmentInteraction(MetaData meta);
@@ -33,7 +38,6 @@ public class AndroidReaderGroup extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_android_reader_group,
 				container, false);
 	}
@@ -52,31 +56,46 @@ public class AndroidReaderGroup extends Fragment {
 		listener = null;
 	}
 
-	public void fill(final Reader reader, final String source) {
-		new AsyncTask<Void, Void, List<MetaData>>() {
+	public void fill(final List<MetaData> metas, final Reader reader) {
+		final List<MetaData> datas = new ArrayList<MetaData>(metas);
+
+		ListView list = getView().findViewById(R.id.Group_root);
+		list.setAdapter(new BaseAdapter() {
 			@Override
-			protected List<MetaData> doInBackground(Void... voids) {
-				return reader.getLibrary().getListBySource(source);
+			public int getCount() {
+				return datas.size();
 			}
 
 			@Override
-			protected void onPostExecute(List<MetaData> metas) {
-				for (MetaData meta : metas) {
-					String tag = "Book_" + meta.getLuid();
-					tag = null; // TODO: how does it work?
-					AndroidReaderBook book = null;// (AndroidReaderBook)
-													// getFragmentManager().findFragmentByTag(tag);
-					if (book == null) {
-						book = new AndroidReaderBook();
-						FragmentTransaction trans = getFragmentManager()
-								.beginTransaction();
-						trans.add(R.id.AndroidReaderGroup_root, book, tag);
-						trans.commit();
-						getFragmentManager().executePendingTransactions();
-					}
-					book.fill(reader, meta.getLuid());
-				}
+			public long getItemId(int position) {
+				return -1; // TODO: what is a "row id" in this context?
 			}
-		}.execute();
+
+			@Override
+			public Object getItem(int position) {
+				return datas.get(position);
+			}
+
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				AndroidReaderBook book = books.get(convertView);
+				if (book == null) {
+					book = new AndroidReaderBook();
+
+					FragmentTransaction trans = getFragmentManager()
+							.beginTransaction();
+					trans.add(book, null);
+					trans.commit();
+					getFragmentManager().executePendingTransactions();
+
+					books.put(book.getView(), book);
+				}
+
+				MetaData meta = (MetaData) getItem(position);
+				book.fill(meta, reader);
+
+				return book.getView();
+			}
+		});
 	}
 }
