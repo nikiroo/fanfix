aac7079dc723e0c03a6c6f58a81d4ef756811fa2
Niki Roo <niki@nikiroo.be>
2018-08-10 17:51:20 +0200

    Try 2

--- src/be/nikiroo/fanfix/library/RemoteLibraryServer.java
+++ src/be/nikiroo/fanfix/library/RemoteLibraryServer.java
@@ -178,6 +178,8 @@ public class RemoteLibraryServer extends ServerObject {
 	 * @return the list of {@link Object}s
 	 */
 	static List<Object> breakStory(Story story) {
+		System.out.println("Story to break: "+story);
+		
 		List<Object> list = new ArrayList<Object>();
 
 		story = story.clone();
@@ -218,6 +220,7 @@ public class RemoteLibraryServer extends ServerObject {
 			}
 		}
 
+		System.out.println("rebuilt story: "+story);
 		return story;
 	}
 
