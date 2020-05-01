5cf61f350b436825d28f5f4f50d8e86fbe5485f1
Niki Roo <niki@nikiroo.be>
2020-05-01 21:43:28 +0200

    e621: use api key

--- supported/E621.java
+++ supported/E621.java
@@ -1,7 +1,6 @@
 package be.nikiroo.fanfix.supported;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,17 +13,20 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.helper.DataUtil;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import be.nikiroo.fanfix.Instance;
+import be.nikiroo.fanfix.bundles.Config;
 import be.nikiroo.fanfix.data.MetaData;
-import be.nikiroo.utils.IOUtils;
 import be.nikiroo.utils.Image;
 import be.nikiroo.utils.Progress;
 import be.nikiroo.utils.StringUtils;
+import be.nikiroo.utils.Version;
 
 /**
  * Support class for <a href="http://e621.net/">e621.net</a> and
@@ -44,7 +46,8 @@ class E621 extends BasicSupport {
 			host = host.substring("www.".length());
 		}
 
-		return ("e621.net".equals(host) || "e926.net".equals(host)) && (isPool(url) || isSearchOrSet(url));
+		return ("e621.net".equals(host) || "e926.net".equals(host))
+				&& (isPool(url) || isSearchOrSet(url));
 	}
 
 	@Override
@@ -58,7 +61,7 @@ class E621 extends BasicSupport {
 
 		meta.setTitle(getTitle());
 		meta.setAuthor(getAuthor());
-		meta.setDate("");
+		meta.setDate(getDate());
 		meta.setTags(getTags());
 		meta.setSource(getType().getSourceName());
 		meta.setUrl(getSource().toString());
@@ -79,8 +82,11 @@ class E621 extends BasicSupport {
 	protected String getDesc() throws IOException {
 		if (isSearchOrSet(getSource())) {
 			StringBuilder builder = new StringBuilder();
-			builder.append("A collection of images from ").append(getSource().getHost()).append("\n") //
-					.append("\tTime of creation: " + StringUtils.fromTime(new Date().getTime())).append("\n") //
+			builder.append("A collection of images from ")
+					.append(getSource().getHost()).append("\n") //
+					.append("\tTime of creation: "
+							+ StringUtils.fromTime(new Date().getTime()))
+					.append("\n") //
 					.append("\tTags: ");//
 			for (String tag : getTags()) {
 				builder.append("\t\t").append(tag);
@@ -100,72 +106,81 @@ class E621 extends BasicSupport {
 	}
 
 	@Override
-	protected List<Entry<String, URL>> getChapters(Progress pg) throws IOException {
+	protected List<Entry<String, URL>> getChapters(Progress pg)
+			throws IOException {
+		int i = 1;
+		String jsonUrl = getJsonUrl();
+		if (jsonUrl != null) {
+			for (i = 1; true; i++) {
+				if (i > 1) {
+					try {
+						// The API does not accept more than 2 request per sec,
+						// and asks us to limit at one per sec when possible
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+					}
+				}
+
+				try {
+					JSONObject json = getJson(jsonUrl + "&page=" + i, false);
+					if (!json.has("posts"))
+						break;
+					JSONArray posts = json.getJSONArray("posts");
+					if (posts.isEmpty())
+						break;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			// The last page was empty:
+			i--;
+		}
+
+		// The pages and images are in reverse order on /posts/
 		List<Entry<String, URL>> chapters = new LinkedList<Entry<String, URL>>();
-		
-		if (isPool(getSource())) {
-			String baseUrl = "https://e621.net/" + getSource().getPath() + "?page=";
-			chapters = getChapters(getSource(), pg, baseUrl, "");
-		} else if (isSearchOrSet(getSource())) {
-			String baseUrl = "https://e621.net/posts/?page=";
-			String search = "&tags=" + getTagsFromUrl(getSource());
-
-			chapters = getChapters(getSource(), pg,
-					baseUrl, search);
+		for (int page = i; page > 0; page--) {
+			chapters.add(new AbstractMap.SimpleEntry<String, URL>(
+					"Page " + Integer.toString(i - page + 1),
+					new URL(jsonUrl + "&page=" + page)));
 		}
 
-		// sets and some pools are sorted in reverse order on the website
-		if (getSource().getPath().startsWith("/posts")) {
-			Collections.reverse(chapters);	
-		}
-		
 		return chapters;
 	}
 
-	private List<Entry<String, URL>> getChapters(URL source, Progress pg, String baseUrl, String parameters)
-			throws IOException {
-		List<Entry<String, URL>> urls = new ArrayList<Entry<String, URL>>();
-
-		if (source.getHost().contains("e926")) {
-			baseUrl = baseUrl.replace("e621", "e926");
-		}
-
-		for (int i = 1; true; i++) {
-			URL url = new URL(baseUrl + i + parameters);
-			try {
-				InputStream pageI = Instance.getInstance().getCache().open(url, this, false);
-				try {
-					if (IOUtils.readSmallStream(pageI).contains("Nobody here but us chickens!")) {
-						break;
-					}
-					urls.add(new AbstractMap.SimpleEntry<String, URL>("Page " + Integer.toString(i), url));
-				} finally {
-					pageI.close();
-				}
-			} catch (Exception e) {
-				break;
-			}
-		}
-
-		return urls;
-	}
-
 	@Override
-	protected String getChapterContent(URL chapUrl, int number, Progress pg) throws IOException {
+	protected String getChapterContent(URL chapUrl, int number, Progress pg)
+			throws IOException {
 		StringBuilder builder = new StringBuilder();
-		Document chapterNode = loadDocument(chapUrl);
-		
-		Elements articles = chapterNode.getElementsByTag("article");
-		
-		// sets and some pools are sorted in reverse order on the website
-		if (getSource().getPath().startsWith("/posts")) {
-			Collections.reverse(articles);	
+
+		JSONObject json = getJson(chapUrl, false);
+		JSONArray postsArr = json.getJSONArray("posts");
+
+		// The pages and images are in reverse order on /posts/
+		List<JSONObject> posts = new ArrayList<JSONObject>(postsArr.length());
+		for (int i = postsArr.length() - 1; i >= 0; i--) {
+			Object o = postsArr.get(i);
+			if (o instanceof JSONObject)
+				posts.add((JSONObject) o);
 		}
-		
-		for (Element el : articles) {
-			builder.append("[");
-			builder.append(el.attr("data-file-url"));
-			builder.append("]<br/>");
+
+		for (JSONObject post : posts) {
+			if (!post.has("file"))
+				continue;
+			JSONObject file = post.getJSONObject("file");
+			if (!file.has("url"))
+				continue;
+
+			try {
+				String url = file.getString("url");
+				builder.append("[");
+				builder.append(url);
+				builder.append("]<br/>");
+			} catch (JSONException e) {
+				// Can be NULL if filtered
+				// When the value is NULL, we get an exception
+				// but the "has" method still returns true
+			}
 		}
 
 		return builder.toString();
@@ -191,11 +206,13 @@ class E621 extends BasicSupport {
 				// Cannot happen
 			}
 		}
-		
+
 		if (isSetOriginalUrl(source)) {
 			try {
-				Document doc = DataUtil.load(Instance.getInstance().getCache().open(source, this, false), "UTF-8", source.toString());
-				for (Element shortname : doc.getElementsByClass("set-shortname")) {
+				Document doc = DataUtil.load(Instance.getInstance().getCache()
+						.open(source, this, false), "UTF-8", source.toString());
+				for (Element shortname : doc
+						.getElementsByClass("set-shortname")) {
 					for (Element el : shortname.getElementsByTag("a")) {
 						if (!el.attr("href").isEmpty())
 							return new URL(el.absUrl("href"));
@@ -208,12 +225,120 @@ class E621 extends BasicSupport {
 
 		if (isPool(source)) {
 			try {
-				return new URL(source.toString().replace("/pool/show/", "/pools/"));
+				return new URL(
+						source.toString().replace("/pool/show/", "/pools/"));
 			} catch (MalformedURLException e) {
 			}
 		}
 
 		return super.getCanonicalUrl(source);
+	}
+
+	private String getTitle() {
+		String title = "";
+
+		Element el = getSourceNode().getElementsByTag("title").first();
+		if (el != null) {
+			title = el.text().trim();
+		}
+
+		for (String s : new String[] { "e621", "-", "e621", "Pool", "-" }) {
+			if (title.startsWith(s)) {
+				title = title.substring(s.length()).trim();
+			}
+			if (title.endsWith(s)) {
+				title = title.substring(0, title.length() - s.length()).trim();
+			}
+		}
+
+		if (isSearchOrSet(getSource())) {
+			title = title.isEmpty() ? "e621" : "[e621] " + title;
+		}
+
+		return title;
+	}
+
+	private String getAuthor() {
+		List<String> list = new ArrayList<String>();
+		String jsonUrl = getJsonUrl();
+		if (jsonUrl != null) {
+			try {
+				JSONObject json = getJson(jsonUrl, false);
+				JSONArray posts = json.getJSONArray("posts");
+				for (Object obj : posts) {
+					if (!(obj instanceof JSONObject))
+						continue;
+
+					JSONObject post = (JSONObject) obj;
+					if (!post.has("tags"))
+						continue;
+
+					JSONObject tags = post.getJSONObject("tags");
+					if (!tags.has("artist"))
+						continue;
+
+					JSONArray artists = tags.getJSONArray("artist");
+					for (Object artist : artists) {
+						if (list.contains(artist.toString()))
+							continue;
+
+						list.add(artist.toString());
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		StringBuilder builder = new StringBuilder();
+		for (String artist : list) {
+			if (builder.length() > 0) {
+				builder.append(", ");
+			}
+			builder.append(artist);
+		}
+
+		return builder.toString();
+	}
+
+	private String getDate() {
+		String jsonUrl = getJsonUrl();
+		if (jsonUrl != null) {
+			try {
+				JSONObject json = getJson(jsonUrl, false);
+				JSONArray posts = json.getJSONArray("posts");
+				for (Object obj : posts) {
+					if (!(obj instanceof JSONObject))
+						continue;
+
+					JSONObject post = (JSONObject) obj;
+					if (!post.has("created_at"))
+						continue;
+
+					return post.getString("created_at");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return "";
+	}
+
+	// no tags for pools
+	private List<String> getTags() {
+		List<String> tags = new ArrayList<String>();
+		if (isSearchOrSet(getSource())) {
+			String str = getTagsFromUrl(getSource());
+			for (String tag : str.split("\\+")) {
+				try {
+					tags.add(URLDecoder.decode(tag.trim(), "UTF-8").trim());
+				} catch (UnsupportedEncodingException e) {
+				}
+			}
+		}
+
+		return tags;
 	}
 
 	// returns "xxx+ddd+ggg" if "tags=xxx+ddd+ggg" was present in the query
@@ -239,102 +364,6 @@ class E621 extends BasicSupport {
 		return tags;
 	}
 
-	private String getTitle() {
-		String title = "";
-
-		Element el = getSourceNode().getElementsByTag("title").first();
-		if (el != null) {
-			title = el.text().trim();
-		}
-
-		for (String s : new String[] { "e621", "-", "e621", "Pool", "-" }) {
-			if (title.startsWith(s)) {
-				title = title.substring(s.length()).trim();
-			}
-			if (title.endsWith(s)) {
-				title = title.substring(0, title.length() - s.length()).trim();
-			}
-		}
-
-		if (isSearchOrSet(getSource())) {
-			title = title.isEmpty() ? "e621" : "[e621] " + title;
-		}
-		
-		return title;
-	}
-
-	private String getAuthor() throws IOException {
-		StringBuilder builder = new StringBuilder();
-
-		if (isSearchOrSet(getSource())) {
-			for (Element el : getSourceNode().getElementsByClass("search-tag")) {
-				if (el.attr("itemprop").equals("author")) {
-					if (builder.length() > 0) {
-						builder.append(", ");
-					}
-					builder.append(el.text().trim());
-				}
-			}
-		}
-
-		if (isPool(getSource())) {
-			String desc = getDesc();
-			String descL = desc.toLowerCase();
-
-			if (descL.startsWith("by:") || descL.startsWith("by ")) {
-				desc = desc.substring(3).trim();
-				desc = desc.split("\n")[0];
-
-				String tab[] = desc.split(" ");
-				for (int i = 0; i < Math.min(tab.length, 5); i++) {
-					if (tab[i].startsWith("http"))
-						break;
-					builder.append(" ").append(tab[i]);
-				}
-			}
-
-			if (builder.length() == 0) {
-				try {
-					String poolNumber = getSource().getPath()
-							.substring("/pools/".length());
-					String url = "https://e621.net/posts" + "?tags=pool%3A"
-							+ poolNumber;
-
-					Document page1 = DataUtil.load(Instance.getInstance()
-							.getCache().open(getSource(), null, false), "UTF-8",
-							url);
-					for (Element el : page1.getElementsByClass("search-tag")) {
-						if (el.attr("itemprop").equals("author")) {
-							if (builder.length() > 0) {
-								builder.append(", ");
-							}
-							builder.append(el.text().trim());
-						}
-					}
-				} catch (Exception e) {
-				}
-			}
-		}
-
-		return builder.toString();
-	}
-
-	// no tags for pools
-	private List<String> getTags() {
-		List<String> tags = new ArrayList<String>();
-		if (isSearchOrSet(getSource())) {
-			String str = getTagsFromUrl(getSource());
-			for (String tag : str.split("\\+")) {
-				try {
-					tags.add(URLDecoder.decode(tag.trim(), "UTF-8").trim());
-				} catch (UnsupportedEncodingException e) {
-				}
-			}
-		}
-
-		return tags;
-	}
-
 	private Image getCover() throws IOException {
 		Image image = null;
 		List<Entry<String, URL>> chapters = getChapters(null);
@@ -350,13 +379,44 @@ class E621 extends BasicSupport {
 		return image;
 	}
 
+	// always /posts.json/ url
+	private String getJsonUrl() {
+		String url = null;
+		if (isSearchOrSet(getSource())) {
+			url = getSource().toString().replace("/posts", "/posts.json");
+		}
+
+		if (isPool(getSource())) {
+			String poolNumber = getSource().getPath()
+					.substring("/pools/".length());
+			url = "https://e621.net/posts.json" + "?tags=pool%3A" + poolNumber;
+		}
+
+		if (url != null) {
+			// Note: one way to override the blacklist
+			String login = Instance.getInstance().getConfig()
+					.getString(Config.LOGIN_E621_LOGIN);
+			String apk = Instance.getInstance().getConfig()
+					.getString(Config.LOGIN_E621_APIKEY);
+
+			if (login != null && !login.isEmpty() && apk != null
+					&& !apk.isEmpty()) {
+				url = String.format("%s&login=%s&api_key=%s&_client=%s", url,
+						login, apk, "fanfix-" + Version.getCurrentVersion());
+			}
+		}
+
+		return url;
+	}
+
 	// note: will be removed at getCanonicalUrl()
 	private boolean isSetOriginalUrl(URL originalUrl) {
 		return originalUrl.getPath().startsWith("/post_sets/");
 	}
 
 	private boolean isPool(URL url) {
-		return url.getPath().startsWith("/pools/") || url.getPath().startsWith("/pool/show/");
+		return url.getPath().startsWith("/pools/")
+				|| url.getPath().startsWith("/pool/show/");
 	}
 
 	// set will be renamed into search by canonical url
