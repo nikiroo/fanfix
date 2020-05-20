1f1318b8c0ba0bc472c263e54079cbaa6e303fe0
Niki Roo <niki@nikiroo.be>
2020-05-20 16:28:19 +0200

    weblib: use templates for all html

--- src/be/nikiroo/fanfix/library/web/package-info.java
+++ src/be/nikiroo/fanfix/library/web/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * This package is only used to store web resources for the Fanfix web server.
+ */
 package be.nikiroo.fanfix.library.web;
--- src/be/nikiroo/fanfix/library/web/templates/package-info.java
+++ src/be/nikiroo/fanfix/library/web/templates/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * This package is used by the Fanfix web server to get HTML templates.
+ */
 package be.nikiroo.fanfix.library.web.templates;
